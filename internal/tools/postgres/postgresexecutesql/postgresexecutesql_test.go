1249ac2f5f81206dc2d3dfd44c8b86fc19a7862f
duwenxin <duwenxin@google.com>
2025-06-23 20:19:04 -0400

    update imports

--- internal/tools/postgres/postgresexecutesql/postgresexecutesql_test.go
+++ internal/tools/postgres/postgresexecutesql/postgresexecutesql_test.go
@@ -21,7 +21,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/googleapis/genai-toolbox/internal/server"
 	"github.com/googleapis/genai-toolbox/internal/testutils"
-	"github.com/googleapis/genai-toolbox/internal/tools/postgresexecutesql"
+	"github.com/googleapis/genai-toolbox/internal/tools/postgres/postgresexecutesql"
 )
 
 func TestParseFromYamlExecuteSql(t *testing.T) {
--- internal/tools/spanner/spannerexecutesql/spannerexecutesql_test.go
+++ internal/tools/spanner/spannerexecutesql/spannerexecutesql_test.go
@@ -21,7 +21,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/googleapis/genai-toolbox/internal/server"
 	"github.com/googleapis/genai-toolbox/internal/testutils"
-	"github.com/googleapis/genai-toolbox/internal/tools/spannerexecutesql"
+	"github.com/googleapis/genai-toolbox/internal/tools/spanner/spannerexecutesql"
 )
 
 func TestParseFromYamlExecuteSql(t *testing.T) {
